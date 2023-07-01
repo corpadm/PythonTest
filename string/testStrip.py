cc11ccb583e6c342d53ad825af40d65ea2f73427
zhangxiaojian <zhangxiaojian@family.com>
2023-07-01 15:32:08 +0800

    增加strip函数测试

---
+++ string/testStrip.py
@@ -0,0 +1,9 @@
+'''
+测试 lstrip、rstrip、strip函数
+'''
+
+testStr='     left5space   mid3space   right8space        '
+print(f"source str[{testStr}]")
+print(f"lstrip:[{testStr.lstrip()}]")
+print(f"rstrip:[{testStr.rstrip()}]")
+print(f"strip:[{testStr.strip()}]")
